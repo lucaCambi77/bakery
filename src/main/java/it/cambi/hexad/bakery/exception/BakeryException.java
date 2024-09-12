f297d24fd8a80d7b365a1bd8f27817ac95a692d1
luca <lucacambi77@gmail.com>
2024-09-12 16:29:06 +0200

    fix: clean

--- src/main/java/it/cambi/hexad/bakery/exception/BakeryException.java
+++ src/main/java/it/cambi/hexad/bakery/exception/BakeryException.java
@@ -1,30 +1,18 @@
-/**
- * 
- */
+/** */
 package it.cambi.hexad.bakery.exception;
 
 /**
  * @author luca
- *
  */
 public class BakeryException extends RuntimeException {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 8470179020128749590L;
+  /** Exception message constructor */
+  public BakeryException(String message) {
+    super(message);
+  }
 
-	/**
-	 * Exception message constructor
-	 */
-	public BakeryException(String message) {
-		super(message);
-	}
-
-	/**
-	 * Throwable constructor
-	 */
-	public BakeryException(Throwable e) {
-		super(e);
-	}
+  /** Throwable constructor */
+  public BakeryException(Throwable e) {
+    super(e);
+  }
 }
